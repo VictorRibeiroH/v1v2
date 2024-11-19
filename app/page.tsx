bbc9c3519cbdc26d0b4db05d6e6d0400896b7758
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-11-18 21:57:11 -0300

    minifix

--- app/page.tsx
+++ app/page.tsx
@@ -230,25 +230,15 @@ export default function Home() {
 
         <div className="md:flex items-center justify-between gap-y-4 my-10 gap-x-28 mx-auto">
             <div className="md:w-2/5">
-              <h1 className="text-2xl font-medium text-gray-600 w-4/5">
+              <h1 className="text-2xl font-medium text-gray-600 w-full">
                Marcas de todo o país confiam em nossa agilidade
               </h1>
 
               <div className="flex my-6 gap-x-5 w-full">
-                <div>
-                  <h1 className="text-blue-500 text-3xl md:text-5xl">
-                    + <NumberTicker value={100} /> 
-                    <p className="text-gray-500 text-sm md:text-md">
-                      Clientes satisfeitos
-                    </p>
-                  </h1>
-                </div>
-
-                <div className="w-px bg-gray-300 self-stretch"></div>
-
+                
                 <div className="flex-1 min-w-0">
                   <h1 className="text-blue-500 text-3xl md:text-5xl whitespace-nowrap overflow-hidden">
-                    + <NumberTicker value={50} /> 
+                    <NumberTicker value={50} /> +
                     <p className="text-gray-500 text-sm md:text-md">
                       Projetos finalizados
                     </p>
